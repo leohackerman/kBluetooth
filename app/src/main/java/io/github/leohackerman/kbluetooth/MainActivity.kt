3645f3f8a34d6c7268b7ea4480cef48424501fb5
Leonardo Ortiz <leonardo.ortiz@molekule.com>
2018-10-17 11:07:07 -0600

    Added functionality for discovering devices

--- app/src/main/java/io/github/leohackerman/kbluetooth/MainActivity.kt
+++ app/src/main/java/io/github/leohackerman/kbluetooth/MainActivity.kt
@@ -1,9 +1,23 @@
 package io.github.leohackerman.kbluetooth
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import io.github.leohackerman.bluetooth.KBluetoothManager
+import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest.permission
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import java.util.jar.Manifest
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +27,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        kBluetoothManager.enableBT(this)
-        val devices = kBluetoothManager.getPairedDevices()
-        Log.d("end","end")
 
+        val MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1
+        ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
+
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(mReceiver, filter)
+        start_discovery.setOnClickListener{
+
+            BluetoothAdapter.getDefaultAdapter().startDiscovery()
+        }
+
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val mReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String = intent.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice =
+                            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                }
+            }
+        }
     }
 }
--- bluetooth/src/main/java/io/github/leohackerman/bluetooth/KBluetoothManager.kt
+++ bluetooth/src/main/java/io/github/leohackerman/bluetooth/KBluetoothManager.kt
@@ -3,7 +3,10 @@ package io.github.leohackerman.bluetooth
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 
 class KBluetoothManager {
 
@@ -26,6 +29,9 @@ class KBluetoothManager {
         }
     }
 
+    /**
+     * Returns a set of paired devices
+     */
     fun getPairedDevices():Set<BluetoothDevice>?{
         val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
         pairedDevices?.forEach { device ->
@@ -33,6 +39,29 @@ class KBluetoothManager {
             val deviceHardwareAddress = device.address // MAC address
         }
         return pairedDevices
+    }
+
+    fun discover(activity: Activity){
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        activity.registerReceiver(mReceiver,filter)
+        btAdapter?.startDiscovery()
+    }
+
+    private val mReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String = intent.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice =
+                            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                }
+            }
+        }
     }
 
 }
